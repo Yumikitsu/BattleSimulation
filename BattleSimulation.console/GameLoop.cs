75de7c5eea9db31750d1f32380aab6f9e25ec5cc
Joaquin Lindkvist <joaquinzache@gmail.com>
2024-08-26 11:15:22 +0200

    Add project files.

---
+++ BattleSimulation.console/GameLoop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleSimulation.console
+{
+    public class GameLoop
+    {
+        //Update function containing the entire game loop
+    }
+}
